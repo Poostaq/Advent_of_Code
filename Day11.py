0a8edf080699be9a2cd5b5ae5353443067383000
Poostaq <piootrek.nowicki@gmail.com>
2017-02-16 18:04:54 +0100

    Some new files and modifications

--- Day11.py
+++ Day11.py
@@ -1,18 +1,113 @@
-class Component:
-    def __init__(self, type, radioisotope):
-        self.type = type
+# -*- coding: UTF-8 -*-
+import Queue
+import itertools
+
+class FloorObject:
+    def __init__(self, object_type, radioisotope="none"):
+        self.object_type = object_type
         self.radioisotope = radioisotope
 
-def create_initial_locations():
+    def to_string(self):
+        return self.object_type + self.radioisotope
+
+
+class Snapshot:
+    def __init__(self, actual_locations, elevator_positon):
+        self.actual_locations = actual_locations
+        self.elevator_position = elevator_positon
+
+    def generate_snapshot_stamp(self):
+        snapshot = ""
+        for floor in print_names_of_objects(self.actual_locations):
+            floor = sorted(floor)
+            for element in floor:
+                snapshot += element
+        snapshot += str(self.elevator_position)
+        print(snapshot)
+        return snapshot
+
+
+def create_initial_locations(path_of_a_file):
     file = open(path_of_a_file, 'r')
     data = file.read()
     data = data.split("\n", len(data) - 1)
+    clear_data = []
     for line in data:
-        
+        line = line.replace("The first ", "").replace("The second ", "").replace("floor contains ", "").\
+            replace("The third ", "").replace("The fourth ", "").replace("a ", "").\
+            replace("nothing relevant", "").replace(".", "").replace("-compatible", "").replace("and ", "")
+        line = line.split(", ",)
+        floor_components = []
+        for element in line:
+            if "generator" in element:
+                radioisotope = element.replace(" generator", "")
+                component = FloorObject("generator", radioisotope)
+                floor_components.append(component)
+            elif "microchip" in element:
+                radioisotope = element.replace(" microchip", "")
+                component = FloorObject("microchip", radioisotope)
+                floor_components.append(component)
+            else:
+                pass
+        clear_data.append(floor_components)
+    return clear_data
 
+
+def print_names_of_objects(location_of_objects):
+    names_of_objects = []
+    for floor in location_of_objects:
+        floor_object_names = []
+        floor_object_names.append(" ")
+        for element in floor:
+            floor_object_names.append(element.radioisotope + element.object_type)
+        names_of_objects.append(floor_object_names)
+    return names_of_objects
+
+
+def is_searched_stamp(Snapshot):
+    if len(Snapshot.actual_locations[3]) == 10:
+        return True
+    else:
+        return False
+
+# DO MOŻLIWEJ OPTYMALIZACJI
+def create_possible_combinations(Snapshot):
+    table_to_return = []
+    for combination in itertools.combinations(Snapshot.actual_locations[Snapshot.elevator_position-1],1):
+        table_to_return.append(combination)
+    for combination in itertools.combinations(Snapshot.actual_locations[Snapshot.elevator_position-1],2):
+        table_to_return.append(combination)
+    return table_to_return
+
+def try_moving_element(snapshot, combination, destination_floor):
+    element_locations = snapshot.actual_locations
+    for element in combination:
+        element_locations[snapshot.elevator_position-1].remove(element)
+        element_locations[destination_floor-1].append(element)
+
+
+def check_if_does_not_blow(elements_of_floor):
+    evaluated_floor = elements_of_floor
+    for element in evaluated_floor:
+        for i in range(len(evaluated_floor)):
+            if element.radioisotope == evaluated_floor[i].radioisotope and element.object_type != evaluated_floor[i].object_type:
+                evaluated_floor.remove(element)
+                evaluated_floor.remove(evaluated_floor[i])
+    #DO DOKONCZENIA
 
 def main():
-    input_locations =
-
+    list_of_snapshots = []
+    list_of_snapshot_stamps = []
+    queue_for_snapshots = Queue()
+    input_locations = create_initial_locations("C:\\Users\\Poostaq\\Dysk Google\\Projekty\\Advent of Code\\day11 input")
+    starting_snapshot = Snapshot(input_locations,1,0)
+    queue_for_snapshots.put(starting_snapshot)
+    while queue_for_snapshots.qsize() > 0:
+        evaluated_snapshot = queue_for_snapshots.get()
+        if is_searched_stamp(evaluated_snapshot):
+            print(evaluated_snapshot.number_of_steps)
+            break
+        else:
+            possible_combinations = create_possible_combinations(evaluated_snapshot)
 if __name__ == "__main__":
     main()
--- Day7.py
+++ Day7.py
@@ -1,14 +1,24 @@
 def check_if_ip_supports_tls(path_of_a_file):
     file = open(path_of_a_file, 'r')
     provided_data = file.read()
-    provided_data = provided_data.split("\n", len(data) - 1)
-    supernet_sequences = []
-    hypernet_sequences = []
+    provided_data = provided_data.split("\n", len(provided_data) - 1)
+    number_of_SSL_IP = 0
+    for line in provided_data:
+        split_sequences = split_line_into_sequences(line)
+        supernet_sequences = split_sequences[0]
+        hypernet_sequences = split_sequences[1]
 
+        aba_sequences = check_for_aba(supernet_sequences)
+        bab_sequences = check_for_aba(hypernet_sequences)
+
+        is_SSL = aba_has_bab(aba_sequences, bab_sequences)
+        if is_SSL :
+            number_of_SSL_IP += 1
+    print(number_of_SSL_IP)
 
 def split_line_into_sequences(line):
     temp_string = ""
-    print("Whole Line: " + line)
+    #print("Whole Line: " + line)
     supernet_sequences = []
     hypernet_sequences = []
     current_sequence = "supernet sequence"
@@ -23,43 +33,32 @@ def split_line_into_sequences(line):
                 hypernet_sequences.append(temp_string)
                 current_sequence = "supernet sequence"
             temp_string = ""
+    supernet_sequences.append(temp_string)
     temp_string = ""
     return [supernet_sequences,hypernet_sequences]
 
-
-        """for ip_part_index in range(len(split_IP)):
-            for character_index in range(len(split_IP[ip_part_index])):
-                if len(split_IP[ip_part_index]) >= character_index+4:
-                    if split_IP[ip_part_index][character_index] == split_IP[ip_part_index][character_index+3]:
-                        if split_IP[ip_part_index][character_index+1] == split_IP[ip_part_index][character_index+2]:
-                            if split_IP[ip_part_index][character_index] != split_IP[ip_part_index][character_index+1]:
-                                if (ip_part_index + 2) % 2 == 0:
-                                    split_IP_part_check["bracketed"] += 1
-                                    print("Part " + split_IP[ip_part_index] + "contains abba string - IP MAY BE VIABLE")
-                                else:
-                                    split_IP_part_check["non-bracketed"] += 1
-                                    print("Part " + split_IP[ip_part_index] + "contains abba string - IP NON-VIABLE")
-        print("Checking IP: " + line)
-        if split_IP_part_check["bracketed"] > 0 and split_IP_part_check["non-bracketed"] == 0:
-            ip_with_tsl_support += 1
-            print("IP with TSL support so far: " + str(ip_with_tsl_support))
-
-        split_IP_part_check["non-bracketed"] = 0
-        split_IP_part_check["bracketed"] = 0
-        split_IP = []"""
-
-
-def check_for_aba(sequence):
+def check_for_aba(sequence_list):
     aba_sequences = []
     aba_found = ""
-    for supernet_character_index in range(len(sequence)-2):
-        if sequence[supernet_character_index] == sequence[supernet_character_index+2]:
-            if sequence[supernet_character_index] != sequence[supernet_character_index+1]:
-                aba_found += sequence[supernet_character_index]+sequence[supernet_character_index+1]+sequence[supernet_character_index+2]
-                aba_sequences.append(aba_found)
+    for sequence in sequence_list:
+        for character_index in range(len(sequence)-2):
+            if sequence[character_index] == sequence[character_index+2]:
+                if sequence[character_index] != sequence[character_index+1]:
+                    aba_found += sequence[character_index]+sequence[character_index+1]+sequence[character_index+2]
+                    aba_sequences.append(aba_found)
+                    #print(aba_found)
+                    aba_found = ""
+        aba_found = ""
+    aba_found = ""
     return aba_sequences
 
 def aba_has_bab(aba_sequences,bab_sequences):
+    for aba in aba_sequences:
+        for bab in bab_sequences:
+            if aba[0] == bab[1] and aba[1] == bab[0]:
+                #print ("Dobre wyniki ma "+ aba + " " + bab)
+                return True
+
 
 
 
