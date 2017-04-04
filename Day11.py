1c14ec40d9da3baea83c68bcce3088a964c61ff9
Poostaq <piootrek.nowicki@gmail.com>
2017-04-04 20:44:19 +0200

    Dzień 12 ++ działający i kod do dnia 11 który nie działa (nie znajduje 4 elementów na ostatnim piętrze, jakieś błędy warunków)

--- Day11.py
+++ Day11.py
@@ -2,7 +2,8 @@ import copy
 import itertools
 from multiprocessing import Queue
 
-class ShortestWay():
+
+class ShortestWay:
     actualIsotopesPositions = []
     actualElevatorPosition = 1
     listOfSnapshots = []
@@ -15,17 +16,18 @@ class ShortestWay():
         for line in data:
             line = line.replace("The first ", "").replace("The second ", "").replace("floor contains ", ""). \
                 replace("The third ", "").replace("The fourth ", "").replace("a ", ""). \
-                replace("nothing relevant", "").replace(".", "").replace("-compatible", "").replace("and ", "").replace(",", "")
+                replace("nothing relevant", "").replace(".", "").replace("-compatible", "").replace("and ", "").\
+                replace(",", "")
             line = line.split(" ", )
-            print(line)
+            # #print(line)
             floor_components = []
-            for element_index in range(0,len(line),2):
+            for element_index in range(0, len(line), 2):
                 if len(line[element_index]) > 0:
                     component = [line[element_index],line[element_index+1]]
                     floor_components.append(component)
-                    print("appending " + str(component))
+                    # #print("appending " + str(component))
             self.actualIsotopesPositions.append(floor_components)
-        print(self.actualIsotopesPositions)
+        # #print(self.actualIsotopesPositions)
         self.numberOfSteps = 0
         self.actualElevatorPosition = 1
 
@@ -37,12 +39,19 @@ class ShortestWay():
         radioisotope = 0
         elementType = 1
         for floor in snapshotPositions:
-            for element in floor:
-                snapshot+=element[radioisotope]+"."+element[elementType]+","
-            snapshot = snapshot[:-1]
-            snapshot += ";"
+            # #print(floor)
+            if len(floor) > 0:
+                for element in floor:
+                    snapshot+=element[radioisotope]+"."+element[elementType]+","
+                snapshot = snapshot[:-1]
+                snapshot += ";"
+            else:
+                snapshot += ";"
+            # #print(snapshot)
         snapshot = snapshot[:-1]
-        return [snapshot, snapshotElevator, snapshotSteps]
+        returnedSnapshot = [snapshot, snapshotElevator, snapshotSteps]
+        # print (returnedSnapshot)
+        return returnedSnapshot
 
     def unpackSnapshot(self, snapshot):
         self.actualIsotopesPositions = [[],[],[],[]]
@@ -62,31 +71,34 @@ class ShortestWay():
         self.numberOfSteps = snapshot[2]
 
     def tryMovingElevator(self, direction, carriedElements):
-        #direction to +1 albo -1
+        # #direction to +1 albo -1
         element_locations = copy.deepcopy(self.actualIsotopesPositions)
+        # #print(element_locations)
         elevator_position = self.actualElevatorPosition
         for element in carriedElements:
-            #print(carriedElements)
-            #print(element)
-            #print(element_locations[elevator_position-1])
-            #print(self.actualIsotopesPositions[elevator_position-1])
+            # print(carriedElements)
+            # print(element)
+            # print(element_locations[elevator_position-1])
+            # print(self.actualIsotopesPositions[elevator_position-1])
             element_locations[elevator_position-1].remove(element)
             element_locations[elevator_position-1+direction].append(element)
         if self.doesNothingBlow(element_locations, carriedElements):
+            #print(element_locations)
             return [element_locations, elevator_position+direction]
-        #print("Przenioslem" + str(carriedElements))
+        # print("Przenioslem" + str(carriedElements))
 
     def doesNothingBlow(self, elementsPositions, movedElements):
-        if self.doesNotElevatorBlow(movedElements):
+        if not self.doesNotElevatorBlow(movedElements):
             return False
-        if self.doesNotFloorBlow(elementsPositions):
+        if not self.doesNotFloorBlow(elementsPositions):
             return False
-        #print("Nie wybuchlo przeniesienie " + str(movedElements))
+        # print("Nie wybuchlo przeniesienie " + str(movedElements))
         return True
 
     def doesNotElevatorBlow(self, combination):
         if len(combination) == 2:
             if combination[0][0] != combination[1][0] and combination[0][1] != combination[1][1]:
+                # print("Wybuchla winda")
                 return False
         return True
 
@@ -94,23 +106,22 @@ class ShortestWay():
         for floor in elementsPositions:
             tempList = copy.deepcopy(floor)
             for elementIndex in range(0,len(floor)):
-                #print("Porównuję: " + str(floor[elementIndex]))
+                # print("Porównuję: " + str(floor[elementIndex]))
                 for iterator in range(0,len(floor)):
-                    #print("porównany do: " + str(floor[iterator]))
+                    # print("porównany do: " + str(floor[iterator]))
                     if elementIndex == iterator:
                         continue
-                    if floor[elementIndex][0] == floor[iterator][0]:
-                        if floor[elementIndex] in tempList:
-                            #print("Usuwamy z listy " + str(tempList) + " element " + str(floor[elementIndex]) + " i " + str(floor[iterator]))
-                            tempList.remove(floor[elementIndex])
-                            tempList.remove(floor[iterator])
-            #print(tempList)
+                    if floor[elementIndex][0] == floor[iterator][0] and floor[elementIndex] in tempList:
+                        # print("Usuwamy z listy " + str(tempList) + " element " + str(floor[elementIndex]) + " i " + str(floor[iterator]))
+                        tempList.remove(floor[elementIndex])
+                        tempList.remove(floor[iterator])
+            # print(tempList)
             microchips = list(filter(lambda x: x[1] == "microchip", tempList))
             generators = list(filter(lambda x: x[1] == "generator", floor))
-            #print(microchips)
-            #print(generators)
-            if len(microchips) > 0\
-                and len(generators) > 0:
+            # print(microchips)
+            # print(generators)
+            if len(microchips) > 0 and len(generators) > 0:
+                # print("Wybuchlo pietro")
                 return False
         return True
 
@@ -125,8 +136,8 @@ class ShortestWay():
             tableToReturn.append(combination)
         return tableToReturn
 
-    def isSearchedSnapshot(self):
-        if len(self.actualIsotopesPositions[3]) == 4:
+    def isSearchedSnapshot(self, positionsInSnapshot):
+        if len(positionsInSnapshot[3]) == 4:
             return True
         else:
             return False
@@ -136,46 +147,56 @@ class ShortestWay():
         initialSnapshot = self.generateSnapshot()
         queue_for_snapshots.put(initialSnapshot)
         self.listOfSnapshots.append(initialSnapshot[0])
+        # print(self.listOfSnapshots)
         iterator = 0
         while queue_for_snapshots.qsize() > 0:
             iterator += 1
-            if iterator%10==0:
-                print(iterator)
+            # if iterator%1000 == 0:
+            print("Sprawdzam snapshot " + str(iterator))
             evaluatedSnapshot = queue_for_snapshots.get()
             self.unpackSnapshot(evaluatedSnapshot)
-            if self.isSearchedSnapshot():
-                print(self.numberOfSteps)
-                break
-            else:
-                possible_combinations = self.createPossibleCombinations()
-                for combination in possible_combinations:
-                    if self.actualElevatorPosition > 1:
-                        listOfLocationsAndElevatorPosition = self.tryMovingElevator(-1, combination)
-                        if listOfLocationsAndElevatorPosition != None and \
-                                self.doesNothingBlow(listOfLocationsAndElevatorPosition[0], combination):
-                            if listOfLocationsAndElevatorPosition[0] not in self.listOfSnapshots:
-                                snapshot = self.generateSnapshot(listOfLocationsAndElevatorPosition[0],
-                                                      listOfLocationsAndElevatorPosition[1],
-                                                      self.numberOfSteps+1)
-                                queue_for_snapshots.put(snapshot)
-                                self.listOfSnapshots.append(snapshot)
-                    if self.actualElevatorPosition < 4:
-                        listOfLocationsAndElevatorPosition = self.tryMovingElevator(1, combination)
-                        if listOfLocationsAndElevatorPosition != None:
-                            if self.doesNothingBlow(listOfLocationsAndElevatorPosition[0], combination) \
-                                    and listOfLocationsAndElevatorPosition[0] not in self.listOfSnapshots:
-                                snapshot = self.generateSnapshot(listOfLocationsAndElevatorPosition[0],
-                                                      listOfLocationsAndElevatorPosition[1],
-                                                      self.numberOfSteps + 1)
-                                queue_for_snapshots.put(snapshot)
-                                self.listOfSnapshots.append(snapshot)
-
-
+            # print("Tworze mozliwe kombinacje")
+            possible_combinations = self.createPossibleCombinations()
+            # print(possible_combinations)
+            for combination in possible_combinations:
+                if self.actualElevatorPosition > 1:
+                    # print("Winda jest powyzej 1, sprawdzam dla poruszania w dol")
+                    listOfLocationsAndElevatorPosition = self.tryMovingElevator(-1, combination)
+                    if listOfLocationsAndElevatorPosition != None and\
+                            self.isSearchedSnapshot(listOfLocationsAndElevatorPosition[0]):
+                        print("ILOSC KROKOW = " + str(self.numberOfSteps+1))
+                    if listOfLocationsAndElevatorPosition != None and \
+                            self.doesNothingBlow(listOfLocationsAndElevatorPosition[0], combination):
+                        if listOfLocationsAndElevatorPosition[0] not in self.listOfSnapshots:
+                            snapshot = self.generateSnapshot(listOfLocationsAndElevatorPosition[0],
+                                                  listOfLocationsAndElevatorPosition[1],
+                                                  self.numberOfSteps+1)
+                            queue_for_snapshots.put(snapshot)
+                            self.listOfSnapshots.append(snapshot)
+                if self.actualElevatorPosition < 4:
+                    # print("Winda jest ponizej 4, sprawdzam dla poruszania w dol")
+                    listOfLocationsAndElevatorPosition = self.tryMovingElevator(1, combination)
+                    if listOfLocationsAndElevatorPosition != None and\
+                            self.isSearchedSnapshot(listOfLocationsAndElevatorPosition[0]):
+                        print("ILOSC KROKOW = " + str(self.numberOfSteps+1))
+                    # print(listOfLocationsAndElevatorPosition)
+                    if listOfLocationsAndElevatorPosition != None:
+                        if self.doesNothingBlow(listOfLocationsAndElevatorPosition[0], combination) \
+                                and listOfLocationsAndElevatorPosition[0] not in self.listOfSnapshots:
+                            snapshot = self.generateSnapshot(listOfLocationsAndElevatorPosition[0],
+                                                  listOfLocationsAndElevatorPosition[1],
+                                                  self.numberOfSteps + 1)
+                            queue_for_snapshots.put(snapshot)
+                            self.listOfSnapshots.append(snapshot)
+                            # print("Dodalem " + str(snapshot))
 
 Test = ShortestWay()
-#print(Test.createPossibleCombinations())
-
+# print(Test.createPossibleCombinations())
+# print(Test.actualIsotopesPositions)
 Test.findShortestPath()
 
-#print(Test.generateSnapshot())
-#print(Test.unpackSnapshot('thulium.generator,thulium.microchip,plutonium.generator,strontium.generator,;plutonium.microchip,strontium.microchip,;promethium.generator,promethium.microchip,ruthenium.generator,ruthenium.microchip,;'))
+# print(Test.generateSnapshot())
+# print(Test.unpackSnapshot('thulium.generator,thulium.microchip,plutonium.generator,strontium.generator,;plutonium.microchip,strontium.microchip,;promethium.generator,promethium.microchip,ruthenium.generator,ruthenium.microchip,;'))
+
+#Test.generateSnapshot([[["aaa","microchip"]],[["aaa", "generator"]], [], []],1,1)
+#Test.generateSnapshot([[["aaa","microchip"],["aaa", "generator"]], [], [], []],1,1)
---
+++ Day12++.py
@@ -0,0 +1,50 @@
+class FindCodeDay12:
+    registers = {"a":0,"b":0,"c":1,"d":0}
+
+    def __init__(self):
+
+        opened_file = open("C:\\Users\\Poostaq\\Dysk Google\\Projekty\\Advent of Code\\test input", 'r')
+        data = opened_file.read()
+        data = data.split("\n", len(data) - 1)
+        executedCommand = 0
+        while executedCommand < len(data):
+            line = data[executedCommand].split(" ")
+            if line[0] == "cpy":
+                self.cpy(line[1], line[2])
+            if line[0] == "inc":
+                self.inc(line[1])
+            if line[0] == "dec":
+                self.dec(line[1])
+            if line[0] == "jnz":
+                jump = self.jnz(line[1],line[2])
+                executedCommand += jump
+            executedCommand += 1
+        print(self.registers["a"])
+
+
+
+    def cpy(self,value,register):
+        if value in self.registers:
+            self.registers[register] = self.registers[value]
+            # print("Kopiuje wartosc z " + str(value) + " do " + str(register))
+        else:
+            self.registers[register] = int(value)
+            # print("Kopiuje wartosc " + str(value) + " do " + str(register))
+
+    def inc(self, register):
+        self.registers[register] += 1
+        # print("Zwiekszylem " + str(register))
+
+    def dec(self,register):
+        self.registers[register] -= 1
+        # print("Zmniejszylem " + str(register))
+
+    def jnz(self, register, jump):
+        if register in self.registers:
+            if self.registers[register] != 0:
+                return int(jump)-1
+        elif register != 0:
+            return int(jump) - 1
+        return 0
+
+Test = FindCodeDay12()
